2f750668e3d1f3edefebca6917a5cf88650cedc3
Oudmaijer, S - SPLXL <Stephan.Oudmaijer@klm.com>
2013-07-11 15:25:34 +0200

    Made sonar work. Lots of fixed. Watch out for the bind IP in the MySQL cookbook.

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,5 +1,6 @@
 # installs common sonar plugins used enterprise wide
 #
 
-sonar_plugin "sonar-build-breaker-plugin"
+#sonar_plugin "sonar-build-breaker-plugin"
 sonar_plugin "sonar-taglist-plugin"
+sonar_plugin "sonar-crowd-plugin"
