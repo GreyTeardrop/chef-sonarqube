d05bc5ce0ca4fdfcdcdecf48d253b494bcee4af4
Oudmaijer, S - SPLXL <Stephan.Oudmaijer@klm.com>
2013-08-23 11:38:11 +0200

    Nicer oneliner :-)

--- definitions/default.rb
+++ definitions/default.rb
@@ -1,10 +1,6 @@
 define :sonar_plugin, :version => "1.0", :repo_url => false do
 
-  repo_location = node[:sonar][:plugins_repo]
-
-  if params[:repo_url]
-    repo_location = params[:repo_url]
-  end
+  repo_location = params[:repo_url] ? params[:repo_url] : node[:sonar][:plugins_repo]
 
   remote_file "#{node[:sonar][:dir]}/#{node[:sonar][:plugins_dir]}/#{params[:name]}-#{params[:version]}.jar" do
     source "#{repo_location}/#{params[:name]}/#{params[:version]}/#{params[:name]}-#{params[:version]}.jar"
