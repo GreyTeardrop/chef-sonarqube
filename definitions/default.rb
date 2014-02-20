7fe02cb747b67ce2e5763b3423c5960646f861a0
Nikolay Rybak <mykola.rybak@gmail.com>
2014-02-20 11:20:35 -0800

    sonar_plugin definition: extract variables

--- definitions/default.rb
+++ definitions/default.rb
@@ -1,14 +1,17 @@
 define :sonar_plugin, :version => '1.0', :repo_url => false do
 
-  repo_location = params[:repo_url] ? params[:repo_url] : node[:sonar][:plugins_repo]
+  repo_url = params[:repo_url] ? params[:repo_url] : node[:sonar][:plugins_repo]
+  download_url = "#{repo_url}/#{params[:name]}/#{params[:version]}/#{params[:name]}-#{params[:version]}.jar"
+  download_path = "#{node[:sonar][:dir]}/#{node[:sonar][:downloads_dir]}/#{params[:name]}-#{params[:version]}.jar"
+  install_path = "#{node[:sonar][:dir]}/#{node[:sonar][:plugins_dir]}/#{params[:name]}-#{params[:version]}.jar"
 
-  remote_file "#{node[:sonar][:dir]}/#{node[:sonar][:downloads_dir]}/#{params[:name]}-#{params[:version]}.jar" do
-    source "#{repo_location}/#{params[:name]}/#{params[:version]}/#{params[:name]}-#{params[:version]}.jar"
+  remote_file download_path do
+    source download_url
     owner 'root'
     group 'root'
     mode 00644
     notifies :restart, 'service[sonar]'
-    not_if { ::File.exists?("#{node[:sonar][:dir]}/#{node[:sonar][:plugins_dir]}/#{params[:name]}-#{params[:version]}.jar") }
+    not_if { ::File.exists?(install_path) }
   end
 
 end
