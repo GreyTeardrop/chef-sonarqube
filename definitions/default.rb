7e33a676edf2a5f49da25f1c838e3d68a42a2821
Nikolay Rybak <mykola.rybak@gmail.com>
2014-02-20 12:02:03 -0800

    Add download_url parameter to sonar_plugin definition
    Directly sets url for downloading plugin, to support downloading plugins
    from urls that do not match default layout.

--- definitions/default.rb
+++ definitions/default.rb
@@ -1,7 +1,12 @@
-define :sonar_plugin, :version => '1.0', :repo_url => false do
+define :sonar_plugin, :version => '1.0', :repo_url => false, :download_url => false do
 
-  repo_url = params[:repo_url] ? params[:repo_url] : node[:sonar][:plugins_repo]
-  download_url = "#{repo_url}/#{params[:name]}/#{params[:version]}/#{params[:name]}-#{params[:version]}.jar"
+  if params[:download_url]
+    download_url = params[:download_url]
+  else
+    repo_url = params[:repo_url] ? params[:repo_url] : node[:sonar][:plugins_repo]
+    download_url = "#{repo_url}/#{params[:name]}/#{params[:version]}/#{params[:name]}-#{params[:version]}.jar"
+  end
+
   download_path = "#{node[:sonar][:dir]}/#{node[:sonar][:downloads_dir]}/#{params[:name]}-#{params[:version]}.jar"
   install_path = "#{node[:sonar][:dir]}/#{node[:sonar][:plugins_dir]}/#{params[:name]}-#{params[:version]}.jar"
 
